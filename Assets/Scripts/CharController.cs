e15f5eec8ccad73b5b6156ed09a0e4e3a393f4ed
Mike Wuetherick <mikew@unity3d.com>
2020-09-16 09:41:07 -0700

    simple example camera-relative movement character controller using the new cinemachine 3rd person follow cam extension

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+namespace PixelWizards.GameSystem.Controllers
+{
+    // the actual monobehaviour that you attach to your character. 
+    public class CharController : MonoBehaviour
+    {
+        [Header("References")]
+        public Animator animator;
+        public Transform invisibleCameraOrigin;
+
+        [Header("Config Vars")]
+        public float walkSpeed = 1.0f;
+        public float runSpeed = 6f;
+        public float moveThreshold = 0.1f;
+        public float gravity = 20.0f;
+        
+        private float desiredSpeed = 0f;
+        private float currentSpeed = 0f;
+        
+        private Vector3 moveDirection = Vector3.zero;
+        
+        private Vector3 forward;
+        private Vector3 right;
+        private float hMove;
+        private float vMove;
+        private Quaternion cameraRot;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            // camera-relative movement
+            forward = invisibleCameraOrigin.forward;
+            forward.y = 0;
+            forward = forward.normalized;
+            right = new Vector3(forward.z, 0, -forward.x);
+
+            // grab input
+            hMove = Input.GetAxis("Horizontal");
+            vMove = Input.GetAxis("Vertical");
+
+            // figure out which direction we're going
+            moveDirection = (hMove * right + vMove * forward).normalized;
+
+            // apply move speed - add run / other move modes and apply their movement speed in a similar manner
+            desiredSpeed = walkSpeed;
+            moveDirection *= Mathf.Lerp(currentSpeed, desiredSpeed, Time.deltaTime);
+
+            // apply gravity
+            gravity *= (Time.deltaTime * 50); // multiply delta time by magic # to get gravity working properly. wee
+            // apply gravity
+            moveDirection.y -= gravity;
+
+            // look the way we are moving (rotates to face direction)
+            moveDirection.y = 0f;
+            currentSpeed = moveDirection.magnitude;
+            if (currentSpeed > moveThreshold)
+            {
+                // tell the animation system how fast we are going
+                animator.SetFloat("Speed", currentSpeed);
+
+                // Preserve the camera rotation when we rotate the player
+                cameraRot = invisibleCameraOrigin.rotation;
+
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+
+                invisibleCameraOrigin.rotation = cameraRot;
+            }
+            else
+            {
+                // tell the animation system that we've stopped
+                animator.SetFloat("Speed", 0f);
+            }
+
+            Debug.DrawRay(transform.position, forward, Color.green, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,133 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace PixelWizards.GameSystem.Controllers
+{
+
+    public class ThirdPersonCameraController : MonoBehaviour
+    {
+        [Header("Camera Rotates around this")]
+        public Transform invisibleCameraOrigin;
+        [Header("Our 3rd person Vcam")]
+        public CinemachineVirtualCamera vcam;                   // the main vcam that we're using
+
+        [Header("Vertical Camera Extents")]
+        public float verticalRotateMin = -80f;
+        public float verticalRotateMax = 80f;
+
+        [Header("Camera Movement Multiplier")]
+        public float cameraVerticalRotationMultiplier = 2f;
+        public float cameraHorizontalRotationMultiplier = 2f;
+
+        [Header("Camera Input Values")]
+        public float cameraInputHorizontal;
+        public float cameraInputVertical;
+
+        [Header("Invert Camera Controls")]
+        public bool invertHorizontal = false;
+        public bool invertVertical = false;
+
+        [Header("Toggles which side the camera should start on. 1 = Right, 0 = Left")]
+        public float cameraSide = 1f;
+
+        [Header("Allow toggling left to right shoulder")]
+        public bool allowCameraToggle = true;
+
+        [Header("How fast we should transition from left to right")]
+        public float cameraSideToggleSpeed = 1f;
+
+        private Cinemachine3rdPersonFollow followCam;           // so we can manipulate the 'camera side' property dynamically
+
+        // current camera rotation values
+        private float cameraX = 0f;
+        private float cameraY = 0f;
+
+        // if we are switching sides
+        private bool doCameraSideToggle = false;
+        private float sideToggleTime = 0f;
+        // where we are in the transition from side to side
+        private float desiredCameraSide = 1f;
+
+        private void tart()
+        {
+            if (vcam == null)
+            {
+                // try to grab the vcam from this object
+                vcam = GetComponent<CinemachineVirtualCamera>();
+            }
+            else
+            {
+                Debug.Log("Need to connect your 3rd person vcam to the CameraController!");
+            }
+        }
+
+        private void Update()
+        {
+            // make sure we have a handle to the follow component
+            if (followCam == null)
+            {
+                followCam = vcam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+            }
+
+            cameraInputHorizontal = -Input.GetAxis("Mouse X");
+            cameraInputVertical = Input.GetAxis("Mouse Y");
+            
+            if(allowCameraToggle)
+            {
+                var side = Input.GetButtonDown("CameraSide");
+                if (side)
+                {
+                    doCameraSideToggle = true;
+                }
+
+                if (doCameraSideToggle)
+                {
+                    sideToggleTime = 0f;
+                    cameraSide = followCam.CameraSide;
+                    if (cameraSide > 0.1)
+                    {
+                        desiredCameraSide = 0f;
+                    }
+                    else
+                    {
+                        desiredCameraSide = 1f;
+                    }
+                    doCameraSideToggle = false;
+                }
+
+                followCam.CameraSide = Mathf.Lerp(cameraSide, desiredCameraSide, sideToggleTime);
+                sideToggleTime += cameraSideToggleSpeed * Time.deltaTime;
+
+            }
+
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.blue, 1.0f);
+            Debug.DrawRay(invisibleCameraOrigin.position, invisibleCameraOrigin.forward, Color.red, 1.0f);
+
+            if (invisibleCameraOrigin != null)
+            {
+                if (invertHorizontal)
+                {
+                    cameraX -= cameraVerticalRotationMultiplier * cameraInputVertical;
+                }
+                else
+                {
+                    cameraX += cameraVerticalRotationMultiplier * cameraInputVertical;
+                }
+
+                if (invertVertical)
+                {
+                    cameraY -= cameraHorizontalRotationMultiplier * cameraInputHorizontal;
+                }
+                else
+                {
+                    cameraY += cameraHorizontalRotationMultiplier * cameraInputHorizontal;
+                }
+
+                invisibleCameraOrigin.eulerAngles = new Vector3(cameraX, cameraY, 0.0f);
+            }
+        }
+    }
+}
